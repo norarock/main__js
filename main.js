91e68c62d959b04e49cf4d1867ae5c7b245d161f
norarock <kk.taf.21@gmail.com>
2021-07-01 18:20:19 +0900

    no message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log(dayjs().format('ss秒'));
 
-const addSubtract = dayjs().subtract(1, 'second').format('ss秒');
+const addSubtract = dayjs().subtract(2, 'second').format('ss秒');
 console.log(addSubtract);
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
